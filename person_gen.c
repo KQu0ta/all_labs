3f737b0792e81f06e0982c7999f9682c91aefa27
KQu0ta <kachevskyaa@gmail.com>
2025-03-04 01:30:45 +0300

    Create generaor

--- person_gen.c
+++ person_gen.c
@@ -15,7 +15,7 @@ struct subject  // Предмет
     unsigned int number_of_lectures:8;      
     unsigned int number_of_laboratory:8;    
     unsigned int lesson_sum:8;              
-    
+    attestation status;
 };
 
 
@@ -24,13 +24,98 @@ struct person // Студент
     char fullname[100];
     struct tm birth;        // Крч тут принцип записи в структуру {число, месяц - 1, год -1900} потом буду фиксить
     char number_of_group[20];
-    struct subject lesson;
-    attestation status;
+    struct subject* lesson;
 
 };
 
-void generation(int n){
+// Функция для генерации случайного числа в диапазоне [min, max]
+int random_int(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
 
+// Функция для генерации случайной даты рождения
+// struct tm random_date() {
+//     int month = random_int(1, 12);
+//     int day = 0;
+//     if (month == 28){
+//         person.birth. = random_int(1, 28);
+//     }else if (month == 4 || month == 6 || month == 9 || month == 11 ){
+//         int day = random_int(1, 30); 
+//     }else{
+//         int day = random_int(1, 31); 
+//     }
+//     int year = random_int(1990, 2005); // Год
+//     return 
+//     // sprintf(date, "%02d.%02d.%04d", day, month, year); // Формат: ДД.ММ.ГГГГ
+// }
+
+// Функция для генерации случайного ФИО
+void random_fio(char *fio) {
+    const char *names[] = { "Никита", "Лев", "Виталий" };
+    const char *surnames[] = { "Клыгин", "Попов", "Гужавин" };
+    const char *lastnames[] = { "Александрович", "Алексеевич", "Сергеевич" };
+
+    // Случайный выбор имени, фамилии и отчества
+    sprintf(fio, "%s %s %s", surnames[random_int(0, 2)], names[random_int(0, 2)], lastnames[random_int(0, 2)]);
+}
+
+
+void random_group(char group[20]){
+    group[0] = random_int(65,90);
+    for (int i = 1; i < 17; i++)
+    {
+        group[i] = random_int(65, 90);
+    }
+    group[17] = '_';
+    group[18] = random_int(10,99);    
+}
+
+// Функция для генерации случайного предмета
+void random_lesson(struct person* student, int min_disciplines, int max_disciplines) {
+    const char *lessons_names[] = { "Рыганье", "Пуканье", "Сраканье" };
+
+    if(min_disciplines == NULL && max_disciplines == NULL){
+        min_disciplines = 10, max_disciplines = 20;
+    }
+    
+    int count_of_lessons = random_int(min_disciplines , max_disciplines);
+    student->lesson = (struct Lesson*)calloc(count_of_lessons, sizeof(struct subject));// прикольная строка я вроде понял, вроде не понял
+    
+    for (int i = 0; i < count_of_lessons; i++)
+    {
+        // student->lesson[i].name_of_subject = lessons_names[random_int(0,2)]; Фикс не знаю почему
+        student->lesson[i].number_of_lectures = random_int(0,255);
+        student->lesson[i].number_of_laboratory = random_int(0,255);
+        student->lesson[i].lesson_sum = random_int(0,255);
+        student->lesson[i].status = random_int(0,2);
+    }
+   
+}
+
+
+void generation(int n){
+    struct person arr[10];
+
+    for (int i = 0; i < n; i++)
+    {
+        random_fio(arr[i].fullname);
+        arr[i].birth.tm_mon = random_int(2, 13);// -1 тк time
+        
+        if (arr[i].birth.tm_mon == 2){
+            arr[i].birth.tm_mday = random_int(1, 28);
+
+        }else if (arr[i].birth.tm_mon == 4 || arr[i].birth.tm_mon == 6 || arr[i].birth.tm_mon == 9 || arr[i].birth.tm_mon == 11 ){
+            arr[i].birth.tm_mday = random_int(1, 30); 
+
+        }else{
+            arr[i].birth.tm_mday = random_int(1, 31); 
+        }
+        arr[i].birth.tm_year = random_int(85, 110); //-1900 тк time
+
+        random_group(arr[i].number_of_group);
+        random_lesson(arr[i].lesson, NULL, NULL);
+    }
+    
 }
 
 
